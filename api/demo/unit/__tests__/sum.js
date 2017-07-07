5265834c5c2070583383f29b260d1aae0fc2bcb0
jmsmrgn <hello@jmsmrgn.com>
2017-07-07 00:14:23 -0700

    write sum function using tdd

--- api/demo/unit/__tests__/sum.js
+++ api/demo/unit/__tests__/sum.js
@@ -1,2 +1,19 @@
-// Let's test drive summing numbers
-test('todo!', () => {})
+// sum() -> 0
+// sum(5) -> 5
+// sum(4, 5, 6) -> 15
+import sum from '../sum'
+
+test('passing nothing gives us zero', () => {
+  const result = sum()
+  expect(result).toBe(0)
+})
+
+test('passing a single number returns that number', () => {
+  const result = sum(5)
+  expect(result).toBe(5)
+})
+
+test('passing any number of numbers returns their sum', () => {
+  const result = sum(4, 5, 6)
+  expect(result).toBe(15)
+})
--- api/demo/unit/sum.js
+++ api/demo/unit/sum.js
@@ -1,1 +1,6 @@
 // let's sum some numbers with TDD
+function sum(...numbers) {
+  return numbers.reduce((acc, n) => n + acc, 0)
+}
+
+export default sum
