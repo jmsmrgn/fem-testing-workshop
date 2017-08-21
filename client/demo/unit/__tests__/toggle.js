981986c4494ae64b56ae0184cf3075f7911f7230
jmsmrgn <hello@jmsmrgn.com>
2017-08-21 15:07:24 -0700

    write unit test for react component using snapshot

--- client/demo/unit/__tests__/toggle.js
+++ client/demo/unit/__tests__/toggle.js
@@ -1,1 +1,8 @@
-test('todo', () => {})
+import React from 'react'
+import {render} from 'enzyme'
+import Toggle from '../toggle'
+
+test('the component renders with defaults', () => {
+  const wrapper = render(<Toggle onToggle={() => {}}>I am child</Toggle>)
+  expect(wrapper).toMatchSnapshotWithGlamor()
+})
