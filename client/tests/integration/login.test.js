728ceda6fb2905cd45f99235f1c5eaf3e7002a12
jmsmrgn <hello@jmsmrgn.com>
2017-08-24 12:10:04 -0700

    write client login integration test

--- client/tests/integration/login.test.js
+++ client/tests/integration/login.test.js
@@ -25,7 +25,38 @@
 //
 // Remember that there are some helpers you can
 // get to render the <Login /> with predefined state.
+//
+import axiosMock from 'axios'
 import Login from '../../src/screens/login'
+import {renderApp, sel, flushAllPromises} from './helpers/utils'
+
+test('logs in when the form is submitted', async () => {
+  // setup
+  const setItemMock = jest.spyOn(window.localStorage, 'setItem')
+  const token = 'Luke, I am your father'
+  const user = {password: 'my-password', email: 'me@example.com'}
+  axiosMock.__mock.instance.post.mockImplementation(() => {
+    return Promise.resolve({data: {user: {token}}})
+  })
+  const {history, wrapper} = renderApp({route: '/login'})
+
+  //actions
+  wrapper.find(sel('email')).node.value = user.email
+  wrapper.find(sel('password')).node.value = user.password
+  wrapper.find('form').simulate('submit')
+  await flushAllPromises()
+
+  // assertions
+  expect(axiosMock.__mock.instance.post).toHaveBeenCalledTimes(1)
+  expect(axiosMock.__mock.instance.post).toHaveBeenCalledWith('/users/login', {
+    user,
+  })
+  expect(history.location.pathname).toBe('/')
+  expect(setItemMock).toHaveBeenCalledTimes(1)
+  expect(setItemMock).toHaveBeenCalledWith('jwt', token)
+  setItemMock.mockRestore()
+})
+
 
 //////// Elaboration & Feedback /////////
 // When you've finished with the exercises:
